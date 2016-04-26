7ab30b224a6d9d91f6a874d06baf6b6d24e8a361
Srinivasu R <r.srinivasu7@gmail.com>
2016-04-26 12:11:10 +0530

    Adding Postgress SQL

--- graddygame/settings.py
+++ graddygame/settings.py
@@ -76,9 +76,7 @@ WSGI_APPLICATION = 'graddygame.wsgi.application'
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-		dj_database_url.config(default='sqlite:///db.sqlite')
-    }
+	'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
 }
 
 
